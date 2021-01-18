c29dc46b96d1f1301e99326e41c738fa5c1583b4
Andrea Carrara <andrea.carrara@libero.it>
2021-01-18 23:07:11 +0100

    added ds18b20 support

--- main/main.c
+++ main/main.c
@@ -13,9 +13,19 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "freertos/event_groups.h"
-void initGPIO();
-void loadParameters();
-void wifi_init_sta();
+
+// https://github.com/UncleRus/esp-idf-lib
+// https://esp-idf-lib.readthedocs.io/en/latest/groups/ds18x20.html
+#include <ds18x20.h>
+
+#define GPIO_SENS_PANEL 1
+#define GPIO_SENS_TANK 2
+
+
+extern void initGPIO();
+extern void loadParameters();
+extern void wifi_init_sta();
+extern void simple_ota_example_task(void *pvParameter);
 
 static EventGroupHandle_t s_wifi_event_group;
 
@@ -25,9 +35,27 @@ void app_main(void)
     initGPIO();
     wifi_init_sta();
     xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
+    
+/*     ds18x20_addr_t panel_sens[1];
+    ds18x20_addr_t tank_sens[1];
+    sensor_count = ds18x20_scan_devices(GPIO_SENS_PANEL, panel_sens, 1);
+    if (sensor_count < 1) ESP_LOGE(TAG,"Panel sensor not detected");
+    sensor_count = ds18x20_scan_devices(GPIO_SENS_TANK, tank_sens, 1);
+    if (sensor_count < 1) ESP_LOGE(TAG,"Tank sensor not detected"); */
+    
+    float Tp,Tt;
     while(true) {
         vTaskDelay(1);
-        fgets(url_buf, OTA_URL_SIZE, stdin);
+        //fgets(url_buf, OTA_URL_SIZE, stdin);
+        ds18x20_measure(GPIO_SENS_PANEL, ds18x20_ANY, false);
+        ds18x20_measure(GPIO_SENS_TANK, ds18x20_ANY, false);
+        vTaskDelay(1);
+        ds18x20_read_temperature(GPIO_SENS_PANEL, ds18x20_ANY, &Tp);
+        ds18x20_read_temperature(GPIO_SENS_TANK, ds18x20_ANY, &Tt);
+
             
     }
 }
+
+
+
